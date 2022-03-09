4096dda1ff62d3284a16d19390178b106a0f2f0a
mgrae <51292396+mgrae@users.noreply.github.com>
2022-03-09 18:59:32 +0200

    Update script.js
    update

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function changeCurrency(){
-$.get( "https://v6.exchangerate-api.com/v6/087411d545b0e01f539eb2a6/latest/USD", function( data ) {
-    const ratio = data.conversion_rates.EUR
-    const select = $( ".pricing-terms h6" );
+    $.get('https://v6.exchangerate-api.com/v6/087411d545b0e01f539eb2a6/latest/EUR')
+  .done(function(data) {
+    const ratio = data.conversion_rates.USD
+    const select = $( ".pricing-terms h6" )
     select[0].replaceWith(select[0].outerText.slice(0, -1) * ratio + ' $')
     select[1].replaceWith(select[1].outerText.slice(0, -1) * ratio + ' $')
     select[2].replaceWith(select[2].outerText.slice(0, -3) * ratio + '+ $')
-});
-}
+  })
+  .fail(function() {
+    alert( "error" );
+  })
+};
 changeCurrency()
